789fb8f1831217cc9ac2342691bb4165729a504c
Sampo Pyysalo <sampo.pyysalo@gmail.com>
2019-06-12 11:12:06 +0300

    Initial import

---
+++ truncatetsv.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python3
+
+import sys
+import os
+
+
+def argparser():
+    from argparse import ArgumentParser
+    ap = ArgumentParser()
+    ap.add_argument('-f', '--fields', metavar='INT[,...]',
+                    help='TSV fields to truncate (default: all)')
+    ap.add_argument('-l', '--length', metavar='INT', type=int, required=True,
+                    help='length to truncate to')
+    ap.add_argument('file', metavar='TSV', nargs='+',
+                    help='TSV files')
+    return ap
+
+
+def main(argv):
+    args = argparser().parse_args(argv[1:])
+    if args.fields is not None:
+        args.fields = [int(i) for i in args.fields.split(',')]
+    for fn in args.file:
+        with open(fn) as f:
+            for ln, l in enumerate(f, start=1):
+                l = l.rstrip('\n')
+                fields = l.split('\t')
+                if args.fields is not None:
+                    fields = [f[:args.length] for f in fields]
+                else:
+                    for f in args.fields:
+                        fields[f] = fields[f][:args.length]
+                print('\t'.join(fields))
+
+
+if __name__ == '__main__':
+    sys.exit(main(sys.argv))
